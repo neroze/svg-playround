1f8fa35184e897594b9a156f54b3d6ffdc3da17c
Niraj Maharjan <neerooze@gmail.com>
2016-08-13 22:24:03 +0545

    random movement part one

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var App = {};
-
+App.rect = [];
 App.random = function(_limit){
 	var limit = _limit || 50;
 	return Math.floor((Math.random() * limit) + 1);
@@ -8,14 +8,33 @@ App.random = function(_limit){
 App.create_ellipse = function(){
 	var _w = App.random(100);
 	var _h = App.random(100);
-	var draw = SVG('drawing').size(500, 500).x(App.random());
-	var rect = draw.ellipse(_w, _h).attr({ fill: '#333' }).stroke({ color: '#f06', opacity: 0.6, width: 5 }).x(App.random(300)).y(App.random(200))
+	
+	return draw.ellipse(_w, _h).attr({ fill: '#333' })
+				.stroke({ color: '#f06', opacity: 0.6, width: 5 })
+				.x(App.random(700)).y(App.random(500))
+				.attr('x', App.random(100)+'%').animate().attr('x', App.random(100)+'%')
+				.animate().dmove(App.random(800), App.random(400));
+	
 }
 
 
 jQuery(document).ready(function($) {
-	for (var i = 50 ; i >= 0; i--) {
-		App.create_ellipse();
+	 draw = SVG('drawing').size('100%', '100%');
+	for (var i = 0 ; i <= 50; i++) {
+		App.rect[i] = App.create_ellipse();
 	}
+
+	jQuery(document).ready(function($) {
+		$(document)
+		.on('mousemove', 'body', function(event) {
+			event.preventDefault();
+
+			
+			for (var i = 0; i < App.rect.length ; i++) {
+				App.rect[i].stop().animate().move(App.random(event.pageX),App.random(event.pageY));
+				console.log("I"+i);
+			}
+		});
+	});
 	
 });
