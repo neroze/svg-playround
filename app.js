f07ef2458f24ba58c71162f84b6d782c6088c526
Niraj Maharjan <neerooze@gmail.com>
2016-08-14 19:54:52 +0545

    reset canvas

--- app.js
+++ app.js
@@ -9,13 +9,10 @@ App.create_ellipse = function(){
 	var _w = App.random(100);
 	var _h = App.random(100);
 	var draw = SVG('drawing').size(500, 500).x(App.random());
-	var rect = draw.ellipse(_w, _h).attr({ fill: '#333' }).stroke({ color: '#f06', opacity: 0.6, width: 5 }).x(App.random(300)).y(App.random(200))
 }
 
 
 jQuery(document).ready(function($) {
-	for (var i = 50 ; i >= 0; i--) {
-		App.create_ellipse();
-	}
+	
 	
 });
