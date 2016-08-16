94d0acf20667712f09cea9651c83d5bb731f31c9
Niraj Maharjan <neerooze@gmail.com>
2016-08-16 09:32:11 +0545

    circle testing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var App = {};
-
+var draw;
 App.random = function(_limit){
 	var limit = _limit || 50;
 	return Math.floor((Math.random() * limit) + 1);
@@ -8,10 +8,35 @@ App.random = function(_limit){
 App.create_ellipse = function(){
 	var _w = App.random(100);
 	var _h = App.random(100);
-	var draw = SVG('drawing').size('100%', '100%');
+	 draw = SVG('drawing').size('100%', '100%');
+	 ellipse = draw
+	.ellipse(200, 200)
+	.attr({
+	  	fill: 'blue'
+		, 'fill-opacity': 0.5
+		, stroke: '#FFF'
+		, 'stroke-width': 10
+	})
+
+	 rect = draw.path('M 100 210, L 100 110,L 180 170, C 180 170 150 210 97 210')
+				.fill('#FFF').stroke({ width: 5,color: '#333' })
+				
+
+	var click = function() {
+	  this.animate().move(100, 90).fill({ color: '#f06', opacity:"0.5"}).loop();
+	}
+
+	var clickE = function () {
+		this.animate({speed:4000,reverse:true}).scale(2);
+	}
+
+rect.on('click', click);
+
+ellipse.on('click', clickE)
+
 }
 
 
 jQuery(document).ready(function($) {
-	
+	App.create_ellipse();
 });
