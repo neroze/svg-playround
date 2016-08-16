8e5368f1d5cfde92193f5d944f94b6b3efa6c7fe
Niraj Maharjan <neerooze@gmail.com>
2016-08-16 21:32:41 +0545

    logo part 1

--- app.js
+++ app.js
@@ -20,23 +20,23 @@ App.draw_sun = function() {
 }
 
 App.draw_mountains = function(){
-	var mt1 = draw.path('M 5 500, L 170, 200, L 500 480, L 250 600')
-		.fill('#333').stroke({
+	var mt1 = draw.path('M 5 500, L 100 400, L 200 450, L 210 440, L 110 390, L 130 370, L 225 425, L 235 410, L 145 360, L 165 340, L 260 390, L 275 370')
+		.fill('none').stroke({
 			width: 1,
 			color: '#333'
 		});
 
-	var mt2 = draw.path('M 5 500, L 170, 200, L 250 600')
-		.fill('#3dd').stroke({
-			width: 1,
-			color: '#333'
-		});
+	// var mt2 = draw.path('M 5 500, L 170, 200, L 250 600')
+	// 	.fill('#3dd').stroke({
+	// 		width: 1,
+	// 		color: '#333'
+	// 	});
 
-	var Mtgroup = draw.group();
-	Mtgroup.add(mt1);
-	Mtgroup.add(mt2);
+	// var Mtgroup = draw.group();
+	// Mtgroup.add(mt1);
+	// Mtgroup.add(mt2);
 
-	return Mtgroup;
+	// return Mtgroup;
 
 }
 
@@ -47,44 +47,12 @@ App.init = function() {
 
 	var sun = App.draw_sun();
 	sun.x(50).y(50);
-	sun.animate(20000).x(800);
+	//sun.animate(20000).x(800);
 
-	
-	var mountain3 = App.draw_mountains();
-	mountain3.x(350).y(-120).scale('0.25');
-	mountain3.animate(20000).x(800);
-
-	var mountain2 = App.draw_mountains();
-	mountain2.x(200).y(-100).scale('0.5');
-	mountain2.animate(20000).x(800);
 
 	var mountain = App.draw_mountains();
-	mountain.animate(20000).x(900);
+	//mountain.animate(20000).x(900);
 
-	
-
-	group = draw.group();
-	group.add(ellipse);
-	group.add(trangle);
-	group.x(150).y(50);
-
-	var click = function() {
-		this.animate().move(100, 90).fill({
-			color: '#f06',
-			opacity: "0.5"
-		}).loop();
-	}
-
-	var clickE = function() {
-		this.animate({
-			speed: 4000,
-			reverse: true
-		}).scale(2);
-	}
-
-	trangle.on('click', click);
-
-	ellipse.on('click', clickE)
 
 }
 
